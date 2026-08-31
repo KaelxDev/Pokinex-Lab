27787d3dfd403c0d7d11bce1eea306a80b40875e
Kael <kaikeerrichelli@gmail.com>
2026-08-31 16:50:59 -0300

    style: load improved message readability layer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,6 +10,7 @@ import './MessageTimePatch.css'
 import './AppEdit.css'
 import './UIRefinement.css'
 import './AuthRefinement.css'
+import './MessageReadable.css'
 import AppEdit from './AppEdit.jsx'
 
 createRoot(document.getElementById('root')).render(
