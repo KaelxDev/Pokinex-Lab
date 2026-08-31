dfb7e11b22eb85af32c4533997cdbc818f09bbb6
Kael <kaikeerrichelli@gmail.com>
2026-08-31 00:22:58 -0300

    feat: use contextual copy menu for messages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppFixed.jsx'
 import './AvatarPatch.css'
+import './MessageContextMenu.css'
+import App from './AppFixed.jsx'
+import MessageContextMenu from './MessageContextMenu.jsx'
+
+function Root() {
+  return (
+    <>
+      <App />
+      <MessageContextMenu />
+    </>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
