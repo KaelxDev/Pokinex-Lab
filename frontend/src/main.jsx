adc8d4e97c41d647098929f9b86374a75f730472
Kael <kaikeerrichelli@gmail.com>
2026-09-04 20:28:52 -0300

    refactor: use App as canonical frontend shell

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import AutoMessageScroll from "./components/AutoMessageScroll.jsx";
-import AppEdit from "./AppEdit.jsx";
+import App from "./App.jsx";
 import PrivateDMFeature from "./components/PrivateDMFeature.jsx";
 import DirectMessageNotifier from "./components/DirectMessageNotifier.jsx";
 import DirectMessageNotificationBridge from "./components/DirectMessageNotificationBridge.jsx";
@@ -11,7 +11,7 @@ import DirectMessageNotificationCenter from "./components/DirectMessageNotificat
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AutoMessageScroll>
-      <AppEdit />
+      <App />
     </AutoMessageScroll>
     <PrivateDMFeature />
     <DirectMessageNotifier />
