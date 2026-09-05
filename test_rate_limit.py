22934df6e4a4eb34eb56b49244e277363caf2926
Kael <kaikeerrichelli@gmail.com>
2026-09-05 16:18:09 -0300

    chore: remove misplaced root rate-limit test

--- test_rate_limit.py
+++
@@ -1,8 +0,0 @@
-from app.rate_limit import allow_request
-
-
-def test_rate_limit_allows_until_limit_then_blocks():
-    key = "test-rate-limit"
-    assert allow_request(key, 2, 60) is True
-    assert allow_request(key, 2, 60) is True
-    assert allow_request(key, 2, 60) is False
